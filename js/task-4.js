1c755377c4570db685dc48015d610cf24da2cf14
Viacheslav Mykhailov <viacheslav.leo.mykhailov@gmail.com>
2025-05-02 12:38:39 +0300

    add task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,23 @@
+const refs = {
+    loginForm: document.querySelector('.login-form'),
+}
 
+refs.loginForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const formData = {
+        [refs.loginForm.email.name]: refs.loginForm.email.value.trim(),
+        [refs.loginForm.password.name]: refs.loginForm.password.value.trim(),
+    }
+
+    const isEmptyField = Object.values(formData).some(value => value === '');
+
+    if (isEmptyField) {
+        alert('All form fields must be filled in');
+        return;
+    }
+
+    console.log(formData);
+
+    refs.loginForm.reset();
+});
