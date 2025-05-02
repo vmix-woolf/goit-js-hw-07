ebdcc8acdcd39c129f63cd9b942428a3cd372232
Viacheslav Mykhailov <viacheslav.leo.mykhailov@gmail.com>
2025-05-02 12:49:33 +0300

    add task-5

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,11 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+    .padStart(6, '0')}`;
 }
+
+const buttonEl = document.querySelector('.change-color');
+
+buttonEl.addEventListener('click', () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+});
